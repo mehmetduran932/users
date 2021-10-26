21db0c1aa1a26beba72a03c32b84417a6cd67fd8
mehmetduran932 <48024660+mehmetduran932@users.noreply.github.com>
2021-10-26 23:00:15 +0300

    axios form bitmedi

--- src/AddForm.js
+++ src/AddForm.js
@@ -1,33 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import { Input, Button, FormControl, FormLabel } from "@chakra-ui/react";
+import axios from "axios";
 function AddForm() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [avatar, setAvatar] = useState("");
+
+  function saveOn() {
+    axios
+      .post("https://reqres.in/api/users?delay=1", {
+        email: email,
+        first_name: firstName,
+        last_name: lastName,
+        avatar: avatar
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
   return (
     <>
       <div>
         <FormControl id="first-name" isRequired>
           <FormLabel>First name</FormLabel>
-          <Input placeholder="First name" />
+          <Input
+            placeholder="First name"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
         </FormControl>
       </div>
       <div>
         <FormControl id="last-name" isRequired>
           <FormLabel>Last name</FormLabel>
-          <Input placeholder="Last name" />
+          <Input
+            placeholder="Last name"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
         </FormControl>
       </div>
       <div>
         <FormControl id="email" isRequired>
           <FormLabel>E-Mail</FormLabel>
-          <Input placeholder="E-Mail" />
+          <Input
+            placeholder="E-Mail"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </FormControl>
       </div>
       <div>
         <FormControl id="src" isRequired>
-          <FormLabel>İmage Url</FormLabel>
-          <Input placeholder="Url" />
+          <FormLabel>Image Url</FormLabel>
+          <Input
+            placeholder="Url"
+            value={avatar}
+            onChange={(e) => setAvatar(e.target.value)}
+          />
         </FormControl>
       </div>
-      <Button colorScheme="blue">Add To Api</Button>
+      <Button colorScheme="blue" onClick={() => saveOn()}>
+        Add To Api
+      </Button>
     </>
   );
 }
