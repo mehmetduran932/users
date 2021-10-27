4487cc704ce1319d0090bf2f3cfc6b85773dbc1c
mehmetduran932 <48024660+mehmetduran932@users.noreply.github.com>
2021-10-27 22:13:00 +0300

    Route Güncellendi Liste default geliyor

--- src/App.js
+++ src/App.js
@@ -7,8 +7,12 @@ import RouteForm from "./RouteForm";
 export default function App() {
   return (
     <div className="App">
+      <div>
+        <RouteForm />
+      </div>
+
       <Switch>
-        <Route path="/" exact component={RouteForm}></Route>
+        <Route path="/" exact component={Users}></Route>
         <Route path="/AddForm" exact component={AddForm}></Route>
         <Route path="/Users" exact component={Users}></Route>
       </Switch>
